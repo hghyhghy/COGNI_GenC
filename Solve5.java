bdb7b98e3bafd2fc3597439bc863c9f53c9bc483
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-15 02:24:44 +0530

    Add files via upload

---
+++ Solve5.java
@@ -0,0 +1,74 @@
+
+import java.util.*;
+
+import java.text.*;
+
+
+
+
+public class Solve5 
+
+{
+       
+    public static void main(String[] args)
+    {
+    
+       Scanner sc= new Scanner(System.in);
+
+       System.out.println("Enter the number of month ");
+
+       int month =sc.nextInt();
+
+       switch(month)
+       {
+              case 12:
+
+              case 1:
+
+              case 2:
+
+              System.out.println("Winter ");
+
+              break;
+
+              case 3:
+
+              case 4:
+
+              case 5:
+
+              System.out.println("Spring");
+
+              break;
+
+
+              case 6:
+
+              case 7:
+
+              case 8:
+
+              System.out.println("Summer");
+
+              break;
+
+              case 9:
+
+              case 10:
+
+              case 11:
+
+              System.out.println("Autumn");
+
+              break;
+
+              default:
+
+              System.out.println("Invalid Month");
+
+
+       }
+
+    }
+
+}
