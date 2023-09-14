9470e260e4d4b0727a84ee5e1eeba99b85cd0ea7
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-15 01:29:18 +0530

    Add files via upload

---
+++ Solve3.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+
+import java.text.*;
+
+
+
+
+
+public class Solve3 
+
+{
+
+       
+       public static void main(String[] args) 
+       
+       {
+       
+              Scanner sc= new Scanner(System.in);
+
+              System.out.println("Enter the digits");
+
+              int a= sc.nextInt();
+
+                            int b= sc.nextInt();
+
+                                          int c= sc.nextInt();
+
+                                                        int d= sc.nextInt();
+
+              
+              char q= (char) a;
+
+              char w= (char) b;
+
+              char e= (char) c;
+
+              char r= (char) d;
+
+
+              System.out.println();
+
+              System.out.print(a);
+
+              System.out.println("-" +q);
+
+                            System.out.print(b);
+
+                                          System.out.println("-" +w);
+
+                                        System.out.print(c);
+
+            System.out.println("-" +e);
+
+                                                 System.out.print(d);
+               System.out.println("-" +r);
+
+
+    
+
+                
+
+
+       }
+
+
+}
